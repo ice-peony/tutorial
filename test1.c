84d06ce52cec91042285366d6d47370c80f38b1f
ice-peony <1124334471@qq.com>
2022-07-18 21:35:09 +0800

    修改添加宏定义比较得出小的一个

---
+++ test1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define MIN(a, b) ((a) - (b) > 0 ? b : a )
+
+int main{
+	int a = 1, b = 3;
+	int min = 0;
+	min = MIN(a, b);
+	printf("small one is %d\n", min);
+	printf("hello World!");
+	return 0;
+}
