88060c33d96cb682225eb24b504753223b9a050f
ice-peony <1124334471@qq.com>
2022-08-05 22:31:30 +0800

    在VScode中使用snippet的方法

--- NumOfOne.c
+++ NumOfOne.c
@@ -14,4 +14,12 @@ int main()
     
     printf("1 has : %d\n",i);
     return 0;
+
+     // make by liupengbin
+     // date is (rq)
+    /*使用 snippet 快捷创建自定义的东西，很有用
+        首先 ctrl+shift+p 搜索 configure user snippet
+        选择你所创建 snippet 的范围
+        再在里面根据注释提示写 $ 是变量到时候可以自定义 { ：} 可以创建默认参数*/ 
+    
 }
